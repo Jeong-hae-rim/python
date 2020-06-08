e222c816bd0f49f04d33cc537657c58c45a22e15
jeong-hae-rim <use_mono@naver.com>
2020-06-08 16:39:36 +0900

    0608 오후 수업 list comprehenstion.

---
+++ 09_list_comprehension.py
@@ -0,0 +1,47 @@
+my_list = []
+
+for x in range(10):
+    my_list.append(x ** 2)
+print(my_list)
+
+my_newlist = [x**2 for x in range(10)]
+# 바로 할당하기 때문에 빈 리스트를 만들지 않음
+my_newlist2 = list(x**2 for x in range(10))
+print(my_newlist)
+print(my_newlist2)
+
+# list comprehension with if statements
+numbers = list(range(10, 100, 10))
+print(numbers)
+
+my_numbers_1 = []
+for number in numbers:
+    if number % 4 == 0:
+        my_numbers_1.append(number)
+
+my_list_1 = [number for number in numbers if number % 4 == 0]
+print(my_numbers_1)
+
+
+my_numbers_2 = []
+for number in numbers:
+    if number >= 50:
+        my_numbers_2.append(number + 7)
+    else:
+        my_numbers_2.append(number + 5)
+
+my_numbers_2 = [number + 7 if number >= 50 else number + 5 for number in numbers]
+print(my_numbers_2)
+
+# 조건 표현식
+# true_value <if> 조건식 <else> false_value
+
+
+gugu = []
+for a in range(2, 10):
+    for b in range(1, 10):
+        gugu.append(a * b)
+print(gugu)
+
+gugu_2 = [a * b for a in range(2, 10) for b in range(1, 10)]
+print(gugu_2)
