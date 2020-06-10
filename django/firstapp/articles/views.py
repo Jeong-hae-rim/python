5d57eefbc389fd6de2bd0f53a17add31e1ed8656
jeong-hae-rim <use_mono@naver.com>
2020-06-10 14:06:23 +0900

    django를 이용한 웹 구축 학습.

---
+++ django/firstapp/articles/views.py
@@ -0,0 +1,52 @@
+# django import style guide
+# 1. standard library
+# 2. 3rd party library
+# 3. django
+# 4. local django
+ 
+import random
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def dinner(request):
+    foods = ['족발', '샐러드', '치킨', '연어덮밥']
+    pick = random.choice(foods)
+    context = {
+        'pick': pick,
+    }
+    return render(request, 'dinner.html', context)
+
+
+def randomimg(request):
+    URL = 'https://picsum.photos/200/300.jpg'
+    context = {
+        'URL': URL,
+    }
+    return render(request, 'randomimg.html', context)
+
+
+def hello(request, name):
+    context = {
+        'name': name
+    }
+    return render(request, 'hello.html', context)
+
+
+def introduce(request, name, age):
+    context = {
+        'name': name,
+        'age': age,
+    }
+    return render(request, 'introduce.html', context)
+
+
+def multiplication(request, front, back):
+    result = front * back
+    context = {
+        'result': result,
+    }
+    return render(request, 'multiplication.html', context)
