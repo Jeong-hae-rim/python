d5abede5553934c551e880c00e0067bdacae1675
jeong-hae-rim <usemono33@gmail.com>
2020-06-11 23:11:55 +0900

    만든 함수 직접 출력까지 해봤다.

--- programmers/00_level1.py
+++ programmers/00_level1.py
@@ -2,17 +2,18 @@
 # #a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요.
 #예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
 
-print('a를 입력하세요.')
-print('b를 입력하세요.')
-
 def solution(a, b):
     answer = 0
     if a == b:
         answer = a
+        print(answer)
     elif a < b:
-       answer = sum(range(a, b+1))
+        answer = sum(range(a, b+1))
+        print(answer)
     else:
         answer = sum(range(b, a+1))
+        print(answer)
 
     return answer
 
+solution(6, 10)
