7d806af5fd150fcd8e67e5b304a7fdee2757af56
jeong-hae-rim <use_mono@naver.com>
2020-06-16 14:39:56 +0900

    crud에서 수정/삭제까지 학습 완료 후 안 보고 프로젝트 짜보는 중.

--- django/01_django_orm/crud/articles/views.py
+++ django/01_django_orm/crud/articles/views.py
@@ -39,7 +39,7 @@ def create(request):
     article.save()
 
 
-    return redirect('articles:index')
+    return redirect('articles:detail', article.pk)
     #return render(request, 'articles/index.html')
 
 
@@ -49,3 +49,45 @@ def detail(request, pk):
         'article': article,
     }
     return render(request, 'articles/detail.html', context)
+
+
+def delete(request, pk):
+    #print(request.method)
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        article.delete()
+        return redirect('articles:index')
+    else:
+        return redirect('articles:detail', article.pk)
+
+
+def edit(request, pk):
+    #요청을 보내기만 하기 때문에 다른 처리할 게 없음
+    #다만 어떤 글인지는 알아야 하기 때문에
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/edit.html', context)
+
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    #1. edit에서 보낸 데이터를 받아야 한다.
+
+    '''
+    title = request.POST.get('title') 
+    content = request.POST.get('content')
+
+    article.title = title
+    article.content = content
+    article.save()
+
+    >> 너무 코드가 반복되는 느낌이니 합쳐보자!
+    '''
+
+    article.title = request.POST.get('title') 
+    article.content = request.POST.get('content')
+    article.save() 
+
+    return redirect('articles:detail', article.pk)
