1b1d4ae69de8f4bfb6d08f9a6050c56db5341edb
jeong-hae-rim <usemono33@gmail.com>
2020-07-01 20:08:56 +0900

    status 창 구현 및 ranking 구현 중.

--- django/ihub/apis/urls.py
+++ django/ihub/apis/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('<int:pk>/detail/', views.detail, name='detail'),
     path('search/<search_string>/', views.search, name='search'),
     path('<int:pk>/status/', views.status, name='status'),
-    path('<int:pk>/download/', views.download, name='download')
+    path('<int:pk>/download/', views.download, name='download'),
+    path('<int:pk>/graph/', views.graph, name='graph'),
+    path('ranking/', views.ranking, name='ranking'),
 ]
 
--- django/ihub/apis/views.py
+++ django/ihub/apis/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Api
-from django.http import JsonResponse
-import json
+from django.http import JsonResponse, HttpResponse, Http404
 from django.forms.models import model_to_dict
 from django.core import serializers
-from statuses.models import Status
-import os
 from django.conf import settings
-from django.http import HttpResponse, Http404
-# Create your views here.
 from django.contrib.auth.decorators import login_required
-import urllib.request
-import mimetypes
+from datetime import datetime
+import json, os, urllib.request, mimetypes, time
+from pprint import pprint
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from .models import Api
+from statuses.models import Status
 
+
+# Create your views here.
 def index(request):
     apis = Api.objects.all()
     context = {
@@ -65,3 +66,59 @@ def download(request, pk):
             response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
             return response
     raise Http404
+
+def graph(request, pk):
+    #statues = get_object_or_404(Status, pk=pk)
+    #var data =  [{
+    #    "date": new Date(2020, 07, 01, 10, 1),
+    #    "value":1,
+    #    "status":"정상"
+    #}];
+
+    statues = Status.objects.filter(api_id=pk)
+    # var data =  [{
+    #     "date": "datetime.datetime",
+    #     "value":1,
+    #     "status":"정상"
+    # }]
+    context = {
+        "msg" : "success"
+    }
+    return JsonResponse(context)
+    
+def ranking(request):
+    url = 'http://data.seoul.go.kr/'
+    print('------------------------')
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x1080')
+    options.add_argument("disable-gpu")
+    # 혹은 options.add_argument("--disable-gpu")
+
+    driver = webdriver.Chrome(
+        ChromeDriverManager().install(), chrome_options=options)
+    driver.get(url)
+    time.sleep(1)
+    api_list = driver.find_elements_by_xpath(
+        '//*[@id="tabPopData1"]/ul/li/a')
+    result = []
+    api_rank = 1
+    for api in api_list:
+        # print(dir(api))
+        api_name = api.find_element_by_class_name('bbs-txt').text
+        pprint(api_name)
+        api_url = api.get_attribute('href')
+        api_obj = {
+            'api_name': api_name,
+            'api_url': api_url,
+            'api_rank': api_rank,
+        }
+        result.append(api_obj)
+        api_rank += 1
+    print(result[0].get('api_name'))
+    context = {
+        'msg' : 'success',
+        'result': result,
+    }
+    return JsonResponse(context)
+
