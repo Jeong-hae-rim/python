0ac6c5cafd3d11d4372687f1b22255df7e8d044a
jeong-hae-rim <usemono33@gmail.com>
2020-07-01 18:28:29 +0900

    UI 합치는 중.

--- django/ihub/apis/urls.py
+++ django/ihub/apis/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'apis'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('apis/', views.apis, name='apis'),
+    path('<int:pk>/detail/', views.detail, name='detail'),
+    path('search/<search_string>/', views.search, name='search'),
+    path('<int:pk>/status/', views.status, name='status'),
+    path('<int:pk>/download/', views.download, name='download')
 ]
+
--- django/ihub/apis/views.py
+++ django/ihub/apis/views.py
@@ -1,15 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Api
+from django.http import JsonResponse
+import json
+from django.forms.models import model_to_dict
+from django.core import serializers
+from statuses.models import Status
+import os
+from django.conf import settings
+from django.http import HttpResponse, Http404
 # Create your views here.
+from django.contrib.auth.decorators import login_required
+import urllib.request
+import mimetypes
 
-
-def apis(request):
+def index(request):
     apis = Api.objects.all()
     context = {
         'apis' : apis
     }
-    return render(request, 'apis/apis.html', context)
+    return render(request, 'apis/index.html', context)
 
+def detail(request, pk):
+    # 추후 누적값을 저장할 코드 구현 --> Status app (DB 저장)
+    api = get_object_or_404(Api,pk=pk)
+    #print(api.download_users)
+    context = {
+        'msg' : 'success',
+        'api_name' : api.api_name,
+        'api_url' : api.api_url,
+        'latest_modified_date' : api.latest_modified_date,
+        'copyright' : api.copyright,
+        'copyright_range' : api.copyright_range,
+        'api_file' : api.api_file,
+        'download_users' : api.download_users.all().count()
+    }
 
-def index(request):
-    return render(request, 'apis/index.html')
+    return JsonResponse(context)
+
+def search(request, search_string):
+    api = get_object_or_404(Api, api_name=search_string)
+    context = {
+        'msg' : 'success',
+        'api_pk' : api.pk
+    }
+    return JsonResponse(context) 
+
+def status(request, pk):
+    #status = get_object_or_404(Status, api_id=pk)
+    latest_status = Status.objects.filter(api_id=pk).latest('updated_time')
+    print(latest_status.updated_time)
+    context = {
+        'msg' : 'success',
+        'latest_status' : latest_status.status
+    }
+    return JsonResponse(context) 
+
+def download(request, pk):
+    api = get_object_or_404(Api, pk=pk)
+    file_path = os.path.join(settings.MEDIA_ROOT, api.api_file)
+    print(file_path)
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type=mimetypes.guess_type(file_path)[0])
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+            return response
+    raise Http404
