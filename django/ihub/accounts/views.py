ef756e42cf3c696995918cee626a4d3c276dc055
jeong-hae-rim <usemono33@gmail.com>
2020-07-04 18:51:45 +0900

    download alert 추가, nav/form/index/profile 수정

--- django/ihub/accounts/views.py
+++ django/ihub/accounts/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .forms import CustomUserCreationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 def signup(request):
